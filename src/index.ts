7a054f68b711020c230653201de8a041f4f57d74
sai saran <saransai1996@gmail.com>
2023-08-25 00:10:56 +0530

    added env setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import './config/index.js'
+
 import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello, Express!')
